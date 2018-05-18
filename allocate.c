c3dacb7dc4cc870caaf93bc0a2fb3dd211580b8e
youtangai <youtanagai@gmail.com>
2018-05-18 22:48:39 +0900

    fix algorithm

--- allocate.c
+++ allocate.c
@@ -2,31 +2,40 @@
 #include<stdlib.h>
 #include<unistd.h>
 
-#define NUM (1024)
+#define NUM (8192)
+
+void fill(unsigned int n, int *p) {
+  for (unsigned int i = n-NUM; i < n; i++) {
+    p[i] = 1;
+  }
+}
 
 int main(void) {
 
-    unsigned int n = 1024;
+    unsigned int n = NUM;
     unsigned int upper = 1024 * 1024 * 1024;
-    char *p;
-    char *q;
+    int *p;
+    int *q;
 
-    if ((p = (char *)malloc(n)) == NULL){
+    if ((p = (int *)malloc(n*sizeof(int))) == NULL){
         printf("mallocできません\n");
         exit(EXIT_FAILURE);
     }
 
+    fill(n, p);
+    
     while(1) {
-        usleep(100);
+      usleep(1);
         n += NUM;
         if (n > upper)
             break;
-        if ((q = (char *)realloc(p, n)) == NULL) {
+        if ((q = (int *)realloc(p, n*sizeof(int))) == NULL) {
             printf("reallocできません\n");
             free(p);
             exit(EXIT_FAILURE);
         }
         p = q;
+	fill(n, p);
     }
     free(p);
     printf("finish!!\n");
